17474329a5477a9da20464b91904482c9b6970e9
mpsdo <mpsdo@yahoo.com.br>
2023-11-29 15:23:29 -0300

    Subida sem a multiplicacao

--- Main.py
+++ Main.py
@@ -8,7 +8,6 @@ class CalcLexer(Lexer):
     # Regular expression rules for tokens
     PLUS = r'\+'
     MINUS = r'-'
-    TIMES = r'\*'
     COMMA = r','
     SEMICOLON = r';'
     LBRACKET = r'\['
@@ -35,87 +34,68 @@ class CalcParser(Parser):
 
     precedence = (
         ('left', PLUS, MINUS),
-        ('left', TIMES),
         ('left', TRANSP, INVERSE),
         ('left', LPAREN, RPAREN),
     )
 
-    @_('S PLUS M')
+    @_('S PLUS S')
     def S(self, p):
         print('\n--- S soma ---')
-        result = (p.S[0] + p.M[0], p.S[1] + p.M[1], p.S[2] + p.M[2], p.S[3] + p.M[3])
+        result = (
+            p.S0[0] + p.S1[0],
+            p.S0[1] + p.S1[1],
+            p.S0[2] + p.S1[2],
+            p.S0[3] + p.S1[3]
+        )
         print(f"S = [{result[0]},{result[1]};{result[2]},{result[3]}]")
         return result
 
-    @_('S MINUS M')
+    @_('S MINUS S')
     def S(self, p):
         print('\n--- S subtração ---')
-        result = (p.S[0] - p.M[0], p.S[1] - p.M[1], p.S[2] - p.M[2], p.S[3] - p.M[3])
+        result = (
+            p.S0[0] - p.S1[0],
+            p.S0[1] - p.S1[1],
+            p.S0[2] - p.S1[2],
+            p.S0[3] - p.S1[3]
+        )
         print(f"S = [{result[0]},{result[1]};{result[2]},{result[3]}]")
         return result
 
-    @_('matrix')
-    def M(self, p):
-        print('\n--- M ---')
-        print(f"M = [{p.matrix[0]},{p.matrix[1]};{p.matrix[2]},{p.matrix[3]}]")
-        return p.matrix
-
-    @_('M TIMES matrix')
-    def M(self, p):
-        print('\n--- M vezes ---')
-        result = (
-            (p.M[0] * p.matrix[0] + p.M[1] * p.matrix[2]),
-            (p.M[0] * p.matrix[1] + p.M[1] * p.matrix[3]),
-            (p.M[2] * p.matrix[0] + p.M[3] * p.matrix[2]),
-            (p.M[2] * p.matrix[1] + p.M[3] * p.matrix[3]),
-        )
-        print(f"M = [{result[0]},{result[1]};{result[2]},{result[3]}]")
-        return result
-
-    @_('TRANSP matrix')
-    def matrix(self, p):
+    @_('TRANSP S')
+    def S(self, p):
         print('\n--- operação transposta ---')
-        result = (p.matrix[0], p.matrix[2], p.matrix[1], p.matrix[3])
-        print(f"M = t[{result[0]},{result[1]};{result[2]},{result[3]}]")
+        result = (p.S[0], p.S[2], p.S[1], p.S[3])
+        print(f"S = t[{result[0]},{result[1]};{result[2]},{result[3]}]")
         return result
 
-    @_('INVERSE matrix')
-    def matrix(self, p):
+    @_('INVERSE S')
+    def S(self, p):
         print('\n--- matriz inversa ---')
-        print(f'M = i[{p.matrix[0]},{p.matrix[1]};{p.matrix[2]},{p.matrix[3]}]')
         print('\n--- operação de inversão ---')
-        det = p.matrix[0] * p.matrix[3] - p.matrix[1] * p.matrix[2]
+        det = p.S[0] * p.S[3] - p.S[1] * p.S[2]
         if det == 0:
             print('Matriz não possui inversa')
             return None
         inv_det = 1 / det
         result = (
-            inv_det * p.matrix[3],
-            -inv_det * p.matrix[1],
-            -inv_det * p.matrix[2],
-            inv_det * p.matrix[0],
+            inv_det * p.S[3],
+            -inv_det * p.S[1],
+            -inv_det * p.S[2],
+            inv_det * p.S[0],
         )
-        print(f'M = i[{result[0]},{result[1]};{result[2]},{result[3]}]')
+        print(f"S = i[{result[0]},{result[1]};{result[2]},{result[3]}]")
         return result
 
     @_('LPAREN S RPAREN')
-    def matrix(self, p):
+    def S(self, p):
         print('\n--- () ---')
-        result = p.S
-        print(f"M = [{result[0]},{result[1]};{result[2]},{result[3]}]")
-        return result
+        return p.S
 
     @_('LBRACKET NUMBER COMMA NUMBER SEMICOLON NUMBER COMMA NUMBER RBRACKET')
-    def matrix(self, p):
+    def S(self, p):
         print('\n--- leitura de uma matriz ---')
         result = (p.NUMBER0, p.NUMBER1, p.NUMBER2, p.NUMBER3)
-        print(f"M = [{result[0]},{result[1]};{result[2]},{result[3]}]")
-        return result
-
-    @_('M')
-    def S(self, p):
-        print('\n--- S ---')
-        result = p.M
         print(f"S = [{result[0]},{result[1]};{result[2]},{result[3]}]")
         return result
 
