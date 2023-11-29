e5e8d62709ac48a5dd81a7c020d6c3ec01f041e7
mpsdo <mpsdo@yahoo.com.br>
2023-11-29 14:54:38 -0300

    Subida3

--- Main.py
+++ Main.py
@@ -36,64 +36,88 @@ class CalcParser(Parser):
     precedence = (
         ('left', PLUS, MINUS),
         ('left', TIMES),
-        ('left', TRANSP),
+        ('left', TRANSP, INVERSE),
         ('left', LPAREN, RPAREN),
     )
 
     @_('S PLUS M')
     def S(self, p):
         print('\n--- S soma ---')
-        return (p.S[0] + p.M[0], p.S[1] + p.M[1], p.S[2] + p.M[2], p.S[3] + p.M[3])
+        result = (p.S[0] + p.M[0], p.S[1] + p.M[1], p.S[2] + p.M[2], p.S[3] + p.M[3])
+        print(f"S = [{result[0]},{result[1]};{result[2]},{result[3]}]")
+        return result
 
     @_('S MINUS M')
     def S(self, p):
         print('\n--- S subtração ---')
-        return (p.S[0] - p.M[0], p.S[1] - p.M[1], p.S[2] - p.M[2], p.S[3] - p.M[3])
+        result = (p.S[0] - p.M[0], p.S[1] - p.M[1], p.S[2] - p.M[2], p.S[3] - p.M[3])
+        print(f"S = [{result[0]},{result[1]};{result[2]},{result[3]}]")
+        return result
 
     @_('matrix')
     def M(self, p):
         print('\n--- M ---')
+        print(f"M = [{p.matrix[0]},{p.matrix[1]};{p.matrix[2]},{p.matrix[3]}]")
         return p.matrix
 
     @_('M TIMES matrix')
     def M(self, p):
         print('\n--- M vezes ---')
-        return ((p.M[0] * p.matrix[0] + p.M[1] * p.matrix[2]), 
-                (p.M[0] * p.matrix[1] + p.M[1] * p.matrix[3]), 
-                (p.M[2] * p.matrix[0] + p.M[3] * p.matrix[2]), 
-                (p.M[2] * p.matrix[1] + p.M[3] * p.matrix[3]))
+        result = (
+            (p.M[0] * p.matrix[0] + p.M[1] * p.matrix[2]),
+            (p.M[0] * p.matrix[1] + p.M[1] * p.matrix[3]),
+            (p.M[2] * p.matrix[0] + p.M[3] * p.matrix[2]),
+            (p.M[2] * p.matrix[1] + p.M[3] * p.matrix[3]),
+        )
+        print(f"M = [{result[0]},{result[1]};{result[2]},{result[3]}]")
+        return result
 
     @_('TRANSP matrix')
     def matrix(self, p):
         print('\n--- operação transposta ---')
-        return (p.matrix[0], p.matrix[2], p.matrix[1], p.matrix[3])
+        result = (p.matrix[0], p.matrix[2], p.matrix[1], p.matrix[3])
+        print(f"M = t[{result[0]},{result[1]};{result[2]},{result[3]}]")
+        return result
 
     @_('INVERSE matrix')
     def matrix(self, p):
+        print('\n--- matriz inversa ---')
+        print(f'M = i[{p.matrix[0]},{p.matrix[1]};{p.matrix[2]},{p.matrix[3]}]')
         print('\n--- operação de inversão ---')
         det = p.matrix[0] * p.matrix[3] - p.matrix[1] * p.matrix[2]
         if det == 0:
             print('Matriz não possui inversa')
             return None
         inv_det = 1 / det
-        inverted_matrix = (inv_det * p.matrix[3], -inv_det * p.matrix[1], -inv_det * p.matrix[2], inv_det * p.matrix[0])
-        print('Matriz inversa:', inverted_matrix)
-        return inverted_matrix
+        result = (
+            inv_det * p.matrix[3],
+            -inv_det * p.matrix[1],
+            -inv_det * p.matrix[2],
+            inv_det * p.matrix[0],
+        )
+        print(f'M = i[{result[0]},{result[1]};{result[2]},{result[3]}]')
+        return result
 
     @_('LPAREN S RPAREN')
     def matrix(self, p):
         print('\n--- () ---')
-        return p.S
+        result = p.S
+        print(f"M = [{result[0]},{result[1]};{result[2]},{result[3]}]")
+        return result
 
     @_('LBRACKET NUMBER COMMA NUMBER SEMICOLON NUMBER COMMA NUMBER RBRACKET')
     def matrix(self, p):
-        print('\n--- leitura de uma matrix ---')
-        return (p.NUMBER0, p.NUMBER1, p.NUMBER2, p.NUMBER3)
+        print('\n--- leitura de uma matriz ---')
+        result = (p.NUMBER0, p.NUMBER1, p.NUMBER2, p.NUMBER3)
+        print(f"M = [{result[0]},{result[1]};{result[2]},{result[3]}]")
+        return result
 
     @_('M')
     def S(self, p):
         print('\n--- S ---')
-        return p.M
+        result = p.M
+        print(f"S = [{result[0]},{result[1]};{result[2]},{result[3]}]")
+        return result
 
 if __name__ == '__main__':
     data = input('Digite a sua expressão:')
